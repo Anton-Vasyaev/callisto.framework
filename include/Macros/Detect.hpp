980269b43bbd779059ab72481ce117546d0367cb
Vasyaev Anton <vasyaevaa@gmail.com>
2019-09-22 22:09:34 +0300

    fixed macro detect for windows msvc compiler

--- include/Macros/Detect.hpp
+++ include/Macros/Detect.hpp
@@ -7,7 +7,7 @@
 
 //
 // DETECT OS
-#if defined(_WIN64) or defined(_WIN32)
+#if defined(_WIN64) || defined(_WIN32)
 #define MACRO_DETECT_WINDOWS
 #endif
 
@@ -19,21 +19,21 @@
 //
 // DETECT ARCHITECTURE
 //
-#if defined(__i386__) or defined(_M_IX86) or defined(_MI86)
+#if defined(__i386__) || defined(_M_IX86) || defined(_MI86)
     #define MACRO_DETECT_I386
 #endif
 
-#if defined(_M_AMD64) or defined(__amd64__)
+#if defined(_M_AMD64) || defined(__amd64__)
     #define MACRO_DETECT_AMD64
 #endif
 
-#if defined(MACRO_DETECT_I386) or defined(MACRO_DETECT_AMD64)
+#if defined(MACRO_DETECT_I386) || defined(MACRO_DETECT_AMD64)
     #define MACRO_DETECT_CPUX86
 #endif
 
 
 // DETECT BIT DESIGN
-#if defined(__linux__)
+#if defined(MACRO_DETECT_LINUX)
     #if defined(__amd64__)
         #define MACRO_DETECT_64BIT
     #else
@@ -41,7 +41,7 @@
     #endif
 #endif
 
-#if defined(_WIN64) or defined(_WIN32)
+#if defined(MACRO_DETECT_WINDOWS)
     #if defined(_WIN64)
         #define MACRO_DETECT_64BIT
     #else
