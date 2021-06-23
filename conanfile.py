61c00ed03c5b617ba825482322cbd2b535b98db8
aavasyaev <vasyaevaa@gmail.com>
2021-06-24 01:18:56 +0300

    trying create conan package

--- conanfile.py
+++ conanfile.py
@@ -5,6 +5,9 @@ class CallistoFrameworkConan(ConanFile):
     version = "0.1.0"
     license = "BSD"
     settings = "os", "compiler", "build_type", "arch"
+
+    exports = "*"
+
     options = { 
         "shared" : [True, False]
     }
@@ -13,29 +16,13 @@ class CallistoFrameworkConan(ConanFile):
     }
     generators = "cmake"
 
-    def source(self):
-        import os
-
-        tools.replace_in_file(
-            "CMakeLists.txt",
-            "project(callisto.framework)",
-            '''
-            project(callisto.framework)
-            set(PACKAGE_CREATE TRUE)
-            '''
-        )
-
     def build(self):
         cmake = CMake(self)
         cmake.definitions["CALLISTO_FRAMEWORK_SHARED"] = self.options.shared
-        cmake.configure(build_dir="./")
-        
+        cmake.configure()
         cmake.build()
 
-
-    def package(self):
-        pass
-
     
-    def package(self):
+    def package_info(self):
+        self.cpp_info.includedirs = [ "include" ]
         self.cpp_info.libs = [ "callisto_framework" ]
