faa5f3eab10ef96a0ed143664b548473ee8a2789
aavasyaev <vasyaevaa@gmail.com>
2021-06-24 19:35:40 +0300

    trying...

--- conanfile.py
+++ conanfile.py
@@ -19,10 +19,16 @@ class CallistoFrameworkConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.definitions["CALLISTO_FRAMEWORK_SHARED"] = self.options.shared
+        cmake.definitions["CREATE_PACKAGE"] = "1"
         cmake.configure()
         cmake.build()
 
+    def package(self):
+        self.copy("*.lib",   dst="lib", keep_path=False)
+        self.copy("*.a",     dst="lib", keep_path=False)
+        self.copy("*.dll",   dst="bin", keep_path=False)
+        self.copy("*.so",    dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
     
     def package_info(self):
-        self.cpp_info.includedirs = [ "include" ]
         self.cpp_info.libs = [ "callisto_framework" ]
