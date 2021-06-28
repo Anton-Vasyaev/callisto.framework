da0fe436b9aba4804ace57285bd2b6159251e348
aavasyaev <vasyaevaa@gmail.com>
2021-06-28 19:29:47 +0300

    write first variant of conan recipe

--- conanfile.py
+++ conanfile.py
@@ -16,6 +16,7 @@ class CallistoFrameworkConan(ConanFile):
     }
     generators = "cmake"
 
+
     def build(self):
         cmake = CMake(self)
         cmake.definitions["CALLISTO_FRAMEWORK_SHARED"] = self.options.shared
@@ -23,12 +24,17 @@ class CallistoFrameworkConan(ConanFile):
         cmake.configure()
         cmake.build()
 
+
     def package(self):
+        self.copy("*.h",   dst="include", src="include")
+        self.copy("*.hpp", dst="include", src="include")
+
         self.copy("*.lib",   dst="lib", keep_path=False)
         self.copy("*.a",     dst="lib", keep_path=False)
         self.copy("*.dll",   dst="bin", keep_path=False)
         self.copy("*.so",    dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
     
+
     def package_info(self):
         self.cpp_info.libs = [ "callisto_framework" ]
